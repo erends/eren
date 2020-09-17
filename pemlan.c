3652403a4fefd9c1cd6cf5ed996177f601ac8cfc
erends <erendio007@gmail.com>
2020-09-17 16:12:48 +0700

    first commit

---
+++ pemlan.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+	printf("****** BIODATA ******\n");
+	printf("Nama		: Eren Dio Sefrila\n");
+	printf("NPM		: 19081010173\n");
+	printf("Angkatan	: 2019\n");
+	printf("Asal		: Sidoarjo\n");
+}
+
