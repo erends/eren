7f08b5ea2253f0bb780b2c7cbba2eb32fa2d6ea7
erends <erendio007@gmail.com>
2020-09-23 19:52:41 +0700

    kamu

--- pemlan.c
+++ pemlan.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(){
-	printf("****** BIODATA ******\n");
+	printf("aku sayang kamu bebeb \n");
 	printf("Nama		: Eren Dio Sefrila\n");
 	printf("NPM		: 19081010173\n");
 	printf("Angkatan	: 2019\n");
